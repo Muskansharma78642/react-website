61d6c8e505accacfa91de698fe89eabc1034068e
Muskan <Muskansharma78642@gmail.com>
2022-04-15 11:36:05 +0530

    Logout Page

---
+++ client/src/components/Logout.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import './style.css';
+
+const Logout = () => {
+    useEffect(() => {
+        fetch('/logout', {
+            method: "GET",
+            headers:{
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        }).then((res) => {
+            if(res.status !== 200){
+                const error = new Error(res.error);
+                throw error;
+            }
+        })
+    })
+
+  return (
+    <>
+    <nav id='navbar'>
+        <a href='./registration'>Registeration</a>
+        <a href='./login'>Login</a>
+        <a href='./products'>Products</a>
+        <a href='./checkout'>Checkout</a>
+    </nav>
+    <div className='container'>
+      <h2>You are logged out. Login again to continue shopping!</h2>
+    </div>
+    </> 
+  );
+}
+
+export default Logout;
